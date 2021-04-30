0161c8c00e7212bb68bcded0370acd1ef74ec530
sxidsvit <s.p.antonyuk@gmail.com>
2021-04-30 19:35:17 +0300

    Location: Names & bookmark

--- screens/Location.js
+++ screens/Location.js
@@ -88,6 +88,100 @@ const Location = ({ navigation, appTheme }) => {
         )
     }
 
+    const renderSearchBar = () => {
+        return (
+            <View
+                style={{
+                    flexDirection: 'row',
+                    marginTop: SIZES.radius,
+                    height: 50,
+                    paddingHorizontal: SIZES.padding,
+                    borderRadius: 25,
+                    backgroundColor: COLORS.lightGreen2,
+                    alignItems: 'center'
+                }}
+            >
+                <TextInput
+                    style={{
+                        flex: 1,
+                        height: 50,
+                        color: COLORS.black,
+                        ...FONTS.body3
+                    }}
+                    placeholder="enter your city, state or zip code"
+                    placeholderTextColor={COLORS.lightGray2}
+                />
+                <Image
+                    source={icons.search}
+                    style={{
+                        width: 30,
+                        height: 30,
+                        tintColor: COLORS.lightGray2
+                    }}
+                />
+            </View>
+        )
+    }
+
+    const renderLocationList = () => {
+        return (
+            <FlatList
+                style={{
+                    marginTop: SIZES.radius,
+                    paddingHorizontal: SIZES.radius
+                }}
+                data={dummyData.locations}
+                keyExtractor={item => item.id}
+                showsVerticalScrollIndicator={false}
+                keyboardDismissMode='on-drag'
+                renderItem={({ item }) => (
+                    <TouchableOpacity
+                        style={{
+                            marginBottom: SIZES.radius,
+                            borderRadius: SIZES.radius * 2,
+                            paddingHorizontal: SIZES.padding,
+                            paddingVertical: SIZES.radius,
+                            backgroundColor: appTheme.cardBackgroundColor
+                        }}
+                        onPress={() => navigation.navigate('Order', {
+                            selectedLocation: item
+                        })}
+                    >
+                        {/* Name & Bookmark  */}
+                        <View
+                            style={{
+                                flexDirection: 'row'
+                            }}
+                        >
+                            <Text
+                                style={{ flex: 1, color: appTheme.textColor, ...FONTS.h2 }}
+                            >{item.title}</Text>
+                            <Image
+                                source={item.bookmarked
+                                    ? icons.bookmarkFilled
+                                    : icons.bookmark}
+                                style={{
+                                    height: 20,
+                                    width: 20,
+                                    tintColor: item.bookmarked
+                                        ? COLORS.red2
+                                        : COLORS.white
+                                }}
+                            />
+                        </View>
+                        {/* Addres  */}
+
+                        {/* Operation Hours  */}
+
+                        {/* Buttons  */}
+
+                    </TouchableOpacity>
+                )}
+
+            />
+        )
+    }
+
     return (
         <View style={styles.container}>
             {/* Header  */}
@@ -102,6 +196,8 @@ const Location = ({ navigation, appTheme }) => {
                 padding: SIZES.padding
             }}>
                 {renderTopBarSection()}
+                {renderSearchBar()}
+                {renderLocationList()}
             </View>
         </View>
     )
